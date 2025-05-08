91d930f189c1d04824a16e7fb377a7f61f44725c
Kola <tubi70@yahoo.com>
2025-05-08 13:10:08 -0400

    Create User.java

---
+++ java/com/humrro/model/User.java
@@ -0,0 +1,18 @@
+package com.humrro.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String role; // e.g., "ROLE_ADMIN", "ROLE_USER"
+}
