c68b43c5d128dba58061e0ba9ad911fc26505006
Kola <tubi70@yahoo.com>
2025-05-08 13:14:07 -0400

    Update UserRepository.java

--- java/com/humrro/repository/UserRepository.java
+++ java/com/humrro/repository/UserRepository.java
@@ -1,1 +1,11 @@
+package com.humrro.repository;
+
+import com.humrro.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+}
 
