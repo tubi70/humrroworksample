87920b73badc9e291c97f94b29d45773da73eecc
Kola <tubi70@yahoo.com>
2025-05-08 14:02:58 -0400

    Update Dashboard.js

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -12,25 +12,3 @@ export default function Dashboard() {
   );
 }
 
-// File: client/src/pages/AdminPage.js
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
-export default function AdminPage() {
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    axios.get('/api/user', {
-      headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
-    })
-    .then((res) => setMessage(res.data))
-    .catch(() => setMessage('Access denied'));
-  }, []);
-
-  return (
-    <div>
-      <h2>Admin Page</h2>
-      <p>{message}</p>
-    </div>
-  );
-}
